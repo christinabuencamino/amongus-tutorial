bbafad1f8f2dbc59ba719909f22f79fca171811d
christinabuencamino <c.buencamino123@gmail.com>
2023-02-19 18:07:39 -0500

    initial commit

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const compression = require("compression");
+const PORT = process.env.PORT || 8080;
+const app = express();
+const socketio = require("socket.io");
+module.exports = app;
